6abd1e45f115602ceb4dc98afd9b9835e1eb1917
csh <loveml506@163.com>
2020-02-07 21:46:50 +0800

    增加查询cmspagelist逻辑

---
+++ xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/service/PageService.java
@@ -0,0 +1,60 @@
+package com.xuecheng.manage_cms.service;
+
+import com.xuecheng.framework.domain.cms.CmsPage;
+import com.xuecheng.framework.domain.cms.request.QueryPageRequest;
+import com.xuecheng.framework.model.response.CommonCode;
+import com.xuecheng.framework.model.response.QueryResponseResult;
+import com.xuecheng.framework.model.response.QueryResult;
+import com.xuecheng.manage_cms.dao.CmsPageRepository;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.*;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PageService {
+    @Autowired
+    CmsPageRepository cmsPageRepository;
+
+    public QueryResponseResult findList(int page, int size, QueryPageRequest queryPageRequest) {
+        if (queryPageRequest == null) {
+            queryPageRequest = new QueryPageRequest();
+        }
+        //自定义条件查询
+        //定义条件匹配器
+        ExampleMatcher exampleMatcher = ExampleMatcher.matching()
+                .withMatcher("pageAliase", ExampleMatcher.GenericPropertyMatchers.contains());
+        //条件值对象
+        CmsPage cmsPage = new CmsPage();
+        //设置条件值（站点id）
+        if(StringUtils.isEmpty(queryPageRequest.getSiteId())){
+            cmsPage.setSiteId(queryPageRequest.getSiteId());
+        }
+        //设置模板id作为查询条件
+        if(StringUtils.isEmpty(queryPageRequest.getTemplateId())){
+            cmsPage.setTemplateId(queryPageRequest.getTemplateId());
+        }
+        //设置页面别名作为查询条件
+        if(StringUtils.isEmpty(queryPageRequest.getPageAliase())){
+            cmsPage.setPageAliase(queryPageRequest.getPageAliase());
+        }
+        if(page<=0){
+            page=1;
+        }
+        page = page -1;
+
+        if(size<=0){
+            size = 10;
+        }
+        Pageable pageable = PageRequest.of(page,size);
+        //定义条件对象Example
+        Example example = Example.of(cmsPage,exampleMatcher);
+        Page all = cmsPageRepository.findAll(example, pageable);
+        QueryResult queryResult=new QueryResult();
+        queryResult.setList(all.getContent());//数据列表
+        queryResult.setTotal(all.getTotalElements());//数据总记录数
+        QueryResponseResult queryResponseResult=new QueryResponseResult(CommonCode.SUCCESS,queryResult);
+        return queryResponseResult;
+    }
+
+}
